81b69d723663a9aaa3357a5011ad01c86fae13ae
Kgford <automatedtestsolutions@gmail.com>
2024-08-01 12:20:38 -0400

    updated settings

--- barcode/urls.py
+++ barcode/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url,include
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 from manufacturing import views
@@ -9,6 +9,6 @@ from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',views.index,name='index'),
+    path('',views.index,name='index'),
     path('manufacturing/', include("manufacturing.urls")),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- manufacturing/urls.py
+++ manufacturing/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf.urls import url
-from django.conf.urls.static import static
 from . import views
 from django.conf import settings
 from django.contrib import admin
